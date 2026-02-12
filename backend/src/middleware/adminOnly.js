b9caaeb89eb6d2a85b9e9cd57ebba2dcdc411723
zahraasharifeh <zahraasharifeh2003@gmail.com>
2026-02-12 19:34:14 +0200

    commit

--- backend/src/middleware/adminOnly.js
+++
@@ -1,6 +0,0 @@
-module.exports = function adminOnly(req, res, next) {
-  if (req.user?.role !== "admin") {
-    return res.status(403).json({ message: "Admin access required" });
-  }
-  next();
-};
--- frontend/src/api/auth.js
+++
@@ -1,4 +0,0 @@
-import client from "./client";
-
-export const registerUser = (data) => client.post("/api/auth/register", data);
-export const loginUser = (data) => client.post("/api/auth/login", data);
